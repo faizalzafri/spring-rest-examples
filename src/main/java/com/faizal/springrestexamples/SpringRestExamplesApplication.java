f89e10e753c98e5518cc81234ec1a13c2c12add5
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-17 20:04:57 +0530

    init project

---
+++ src/main/java/com/faizal/springrestexamples/SpringRestExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.faizal.springrestexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestExamplesApplication.class, args);
+	}
+
+}
