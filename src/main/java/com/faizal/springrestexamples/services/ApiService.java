1d2a32bcddad19e93b605287e52aca19b0defed3
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2019-02-18 23:38:07 +0530

    added WebClient and reactive apiService::getUsers

--- src/main/java/com/faizal/springrestexamples/services/ApiService.java
+++ src/main/java/com/faizal/springrestexamples/services/ApiService.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import com.faizal.springrestexamples.api.domain.User;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ApiService {
 
 	List<User> getUsers(Integer limit);
+	
+	Flux<User> getUsers(Mono<Integer> limit);
 }
